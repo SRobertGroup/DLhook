8e226c452e52c5d591b1e67e82713b3350dc3283
HeymansAdrien <adrien.heymans@gmail.com>
2025-06-06 14:44:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 from seedling_measurment import *
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-import torch
-print("torch has access to GPUs Available: ", torch.cuda.is_available())
 
 def main():
     remove_files_exit()
@@ -15,16 +11,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# ---------------
-## Archive 
-# ---------------
-# from seedling_measurment import *
-# 
-# remove_files_exit()
-# root=tk.Tk()
-# gui=Gui(root)
-# w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-# root.geometry("%dx%d+0+0" % (1280, 700))
-# root.mainloop()
